ce551361e54d6915d458d92f0601dd38a1d4f4eb
Syaiful Amin <syaiful@quadrant.io>
2020-10-28 14:06:56 +0700

    update something

--- Package.swift
+++ Package.swift
@@ -10,13 +10,9 @@ let package = Package(
             name: "QDPublisher",
             targets: ["QDPublisher"]),
     ],
-    dependencies: [
-        package(path: ".")
-    ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "QDPublisher",
-            dependencies: [],
             url: "https://raw.githubusercontent.com/syaifulQ/QDPublisher/main/QDPublisher.xcframework.zip",
             checksum: "ceeca766b4251a2105f78b1fd59d1fb8")
     ]

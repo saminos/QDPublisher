f1fb7f69c7b3337237d4f5d76b8c3abb869bfef0
Syaiful Amin <saminos@Syaifuls-MacBook-Pro.local>
2020-10-28 14:39:35 +0700

    update something

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "QDPublisher",
-            targets: ["QDPublisher.xcframework"]),
+            targets: ["QDPublisher"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,7 +18,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        
+        .target(
+            name: "QDPublisher",
+            dependencies: ["QDPublisher.xcframework"]
+        ),
         .binaryTarget(name: "QDPublisher.xcframework", path: "QDPublisher.xcframework")
     ]
 )

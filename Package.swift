2a2d7747459d783bdc911c6ada9b5fdc9763484f
Syaiful Amin <saminos@Syaifuls-MacBook-Pro.local>
2020-10-28 15:01:31 +0700

    update something

--- Package.swift
+++ Package.swift
@@ -8,13 +8,13 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "QDPublisher.xcframework",
-            targets: ["QDPublisher.xcframework"]),
+            name: "QDPublisher",
+            targets: ["QDPublisher"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         
-        .binaryTarget(name: "QDPublisher.xcframework", url: "https://raw.githubusercontent.com/syaifulQ/QDPublisher/main/QDPublisher.xcframework.zip", checksum: "ab4260f4dd5ac33fee77bc251fd4c1b73230dbd5ba8ea2fe44d040120961dcf4")
+        .binaryTarget(name: "QDPublisher", url: "https://raw.githubusercontent.com/syaifulQ/QDPublisher/main/QDPublisher.xcframework.zip", checksum: "ab4260f4dd5ac33fee77bc251fd4c1b73230dbd5ba8ea2fe44d040120961dcf4")
     ]
 )
